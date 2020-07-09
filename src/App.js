b6a44a5e9864173593558b4bf311319363d47fcd
Andrey Slukin <aslukin@gmail.com>
2020-07-10 00:04:32 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+        <h1 className="display-1">Display 1</h1>
+
+        <h1>
+          test
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      </header>
     </div>
   );
 }
